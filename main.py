b90eb98b8e9248c07a07c607a444d835af87847d
Madhulika Mukherjee <madhulika.15nov@gmail.com>
2013-10-19 14:50:41 +0530

    adding structure

--- main.py
+++ main.py
@@ -2,3 +2,12 @@ print("Press\n1 for cube volume\n2 for cube surface area3for cuboid volume\n4 fo
 
 a = int(raw_input("Give choice"));
 print a;
+
+if a==1:
+    cube_volume();
+
+if a==2:
+    cube_surface_area();
+
+if a==3:
+    cuboid_volume();
