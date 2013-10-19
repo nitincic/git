6acdf0a3dc85515e69070ed1b0b57dafb5d9a6e2
Madhulika Mukherjee <madhulika.15nov@gmail.com>
2013-10-19 16:36:47 +0530

    menu and first function

--- main.py
+++ main.py
@@ -6,23 +6,3 @@ print a;
 def cube_volume():
     i=float(raw_input("Enter side length "))
     return i*i*i
-
-def cuboid_volume():
-    l=float(raw_input("Enter length "))
-    b=float(raw_input("Enter width "))
-    h=float(raw_input("Enter height "))
-    return l*b*h
-
-def cube_volume():
-    i=float(raw_input("Enter side length"))
-    return i*i*i
-
-def cube_surface_area():
-    i=float(raw_input("Enter side length "))
-    return 6*i*i
-
-
-
-def cuboid_surface_area():
-    print "the surface area is 25";
-    
