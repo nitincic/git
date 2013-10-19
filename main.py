45d03c0471950b8016bb00ebd0777b28fc0dc6b4
Nitin Agrawal <nitin.cic@gmail.com>
2013-10-19 14:41:58 +0530

    init commit

---
+++ main.py
@@ -0,0 +1,19 @@
+def cube_volume():
+    i=float(raw_input("Enter side length "))
+    return i*i*i
+
+def cuboid_volume():
+    l=float(raw_input("Enter length "))
+    b=float(raw_input("Enter width "))
+    h=float(raw_input("Enter height "))
+    return l*b*h
+
+def cube_volume():
+    i=float(raw_input("Enter side length"))
+    return i*i*i
+
+def cube_surface_area():
+    i=float(raw_input("Enter side length "))
+    return 6*s*s
+
+
